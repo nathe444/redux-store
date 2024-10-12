6e97ba59862cc2b689e1bf0f22959073c4c23f2c
unknown <natnaelm552@gmail.com>
2024-10-12 16:42:46 +0300

    products fetched and listed

---
+++ src/components/Product.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import "./product.css";
+const Product = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <div className="product-container">
+      <h1>Products</h1>
+      <div className="product-grid">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <img
+              className="product-image"
+              src={product.image}
+              alt={product.title}
+            />
+            <div className="product-info">
+              <h3 className="product-title">{product.title}</h3>
+              <div className="product-price">${product.price}</div>
+              <button className="product-button">Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Product;
